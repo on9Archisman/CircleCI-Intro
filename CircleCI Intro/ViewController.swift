d2344b9ffeb68236ad5fd73792da7725d9f5d5ea
Archisman Banerjee <archisman.tech@gmail.com>
2023-02-11 03:21:43 +0530

    Initial Commit

---
+++ CircleCI Intro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleCI Intro
+//
+//  Created by Archisman Banerjee on 11/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CircleCI IntroUITests/CircleCI_IntroUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CircleCI_IntroUITestsLaunchTests.swift
+//  CircleCI IntroUITests
+//
+//  Created by Archisman Banerjee on 11/02/23.
+//
+
+import XCTest
+
+final class CircleCI_IntroUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
